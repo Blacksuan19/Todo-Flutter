d3e168681a861b661ccbc3ccfa6585ea800c3112
blacksuan19 <abubakaryagob@gmail.com>
2021-02-03 21:31:52 +0800

    todo: class cleanup

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:uuid/uuid.dart';
@@ -17,68 +15,6 @@ class Todo {
     this.content,
     this.isCompleted,
   }) : id = _uuid.v4();
-
-  Todo copyWith({
-    String id,
-    String title,
-    String content,
-    bool isCompleted,
-  }) {
-    return Todo(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      content: content ?? this.content,
-      isCompleted: isCompleted ?? this.isCompleted,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-      'content': content,
-      'isCompleted': isCompleted,
-    };
-  }
-
-  factory Todo.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
-
-    return Todo(
-      id: map['id'],
-      title: map['title'],
-      content: map['content'],
-      isCompleted: map['isCompleted'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Todo.fromJson(String source) => Todo.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'Todo(id: $id, title: $title, content: $content, isCompleted: $isCompleted)';
-  }
-
-  @override
-  bool operator ==(Object o) {
-    if (identical(this, o)) return true;
-
-    return o is Todo &&
-        o.id == id &&
-        o.title == title &&
-        o.content == content &&
-        o.isCompleted == isCompleted;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^
-        title.hashCode ^
-        content.hashCode ^
-        isCompleted.hashCode;
-  }
 }
 
 class TodoList extends ChangeNotifier {
